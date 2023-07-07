9cb2a560aa617952c251fd9c61e0a285bd1d2bdd
warckut <warckut@gmail.com>
2023-07-07 17:10:08 +0500

    disabled local eslint-rules

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
