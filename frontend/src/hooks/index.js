920d43828bfca3d467642c65bcf7f395a44b05f3
warckut <warckut@gmail.com>
2023-07-07 17:07:57 +0500

    remove eslint rules

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable functional/no-conditional-statements */
-/* eslint-disable functional/no-expression-statements */
 import { useContext } from 'react';
 
 import authContext from '../context';
