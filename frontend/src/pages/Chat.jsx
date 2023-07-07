128d5418a1ead0e2de78c457bc899226e2d5a463
warckut <warckut@gmail.com>
2023-07-07 17:23:44 +0500

    files moved

--- frontend/src/pages/Chat.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable functional/no-conditional-statements */
-/* eslint-disable functional/no-expression-statements */
-import React from 'react';
-
-const Chat = () => (<h1>CHAT</h1>);
-
-export default Chat;
--- frontend/src/pages/NoMatch.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NoMatch = () => (
-  <div style={{ margin: '100px auto' }}><h1 style={{ textAlign: 'center' }}>Ошибка 404</h1></div>
-);
-
-export default NoMatch;
