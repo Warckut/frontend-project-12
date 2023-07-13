14ac74b508b27905d2f2f3677eba80a1334dd17e
warckut <warckut@gmail.com>
2023-07-13 11:04:33 +0500

    rollbar config

--- frontend/src/rollbar/config.js
+++ frontend/src/rollbar/config.js
@@ -1,10 +1,6 @@
 const config = {
   accessToken: '00c1cc98e4734ce09d2cc3401712e7db',
-  payload: {
-    environment: 'production',
-  },
-  captureUncaught: true,
-  captureUnhandledRejections: true,
+  environment: 'production',
 };
 
 export default config;
