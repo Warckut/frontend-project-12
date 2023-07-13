6c63a0582a5804ac2b337643ee20997ab0e50da2
warckut <warckut@gmail.com>
2023-07-13 09:49:59 +0500

    added rolbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,10 +1,15 @@
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import filter from 'leo-profanity';
 
 import App from './components/App';
 import resources from './locales/index';
 import store from './slices/index';
+import ChatProvider from './context/ChatProvider';
+import AuthProvider from './context/AuthProvider';
+import config from './rollbar/config';
 
 const init = async () => {
   const i18n = i18next.createInstance();
@@ -16,12 +21,23 @@ const init = async () => {
       fallbackLng: 'ru',
     });
 
+  const ruDict = filter.getDictionary('ru');
+  filter.add(ruDict);
+
   return (
-    <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    </Provider>
+    <RollbarProvider config={config}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <I18nextProvider i18n={i18n}>
+            <AuthProvider>
+              <ChatProvider>
+                <App />
+              </ChatProvider>
+            </AuthProvider>
+          </I18nextProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
---
+++ frontend/src/rollbar/config.js
@@ -0,0 +1,10 @@
+const config = {
+  accessToken: '00c1cc98e4734ce09d2cc3401712e7db',
+  payload: {
+    environment: 'production',
+  },
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
+
+export default config;
