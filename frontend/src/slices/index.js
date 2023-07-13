ef8a72b5a1a455034167f2127d342c7e0455f616
warckut <warckut@gmail.com>
2023-07-13 09:50:52 +0500

    added modals slice

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-import channelsReducer from './channelsSlice';
-import messagesReducer from './messagesSlice';
+import channelsReducer, { actions as channelActions } from './channelsSlice';
+import messagesReducer, { actions as messagesActions } from './messagesSlice';
+import modalsReducer, { actions as modalsActions } from './modalSlice';
 
 export default configureStore({
   reducer: {
     channelsInfo: channelsReducer,
     messagesInfo: messagesReducer,
+    modals: modalsReducer,
   },
 });
+
+export const actions = {
+  ...channelActions,
+  ...messagesActions,
+  ...modalsActions,
+};
---
+++ frontend/src/slices/modalSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const modalSlice = createSlice({
+  name: 'modals',
+  initialState: {
+    modalType: null,
+    channelId: null,
+  },
+  reducers: {
+    setCurrentModal(state, { payload }) {
+      state.modalType = payload;
+    },
+    setCurrentChannel(state, { payload }) {
+      state.channelId = payload;
+    },
+  },
+});
+
+export const { actions } = modalSlice;
+export default modalSlice.reducer;
