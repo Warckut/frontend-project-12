5304ddba6771f983fdba32b6f42b5115c7afdb44
warckut <warckut@gmail.com>
2023-07-16 19:19:29 +0500

    added loader slice

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer, { actions as channelActions } from './channelsSlice';
 import messagesReducer, { actions as messagesActions } from './messagesSlice';
 import modalsReducer, { actions as modalsActions } from './modalSlice';
+import loaderReducer from './loaderSlice';
 
 export default configureStore({
   reducer: {
     channelsInfo: channelsReducer,
     messagesInfo: messagesReducer,
     modals: modalsReducer,
+    loader: loaderReducer,
   },
 });
 
---
+++ frontend/src/slices/loaderSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+import fetchUserData from './fetchUserData';
+
+const loaderSlice = createSlice({
+  name: 'loader',
+  initialState: {
+    status: 'IDLE',
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUserData.fulfilled, (state) => {
+        state.status = 'LOADED';
+      })
+      .addCase(fetchUserData.rejected, (state) => {
+        state.status = 'ERROR';
+      });
+  },
+});
+
+export default loaderSlice.reducer;
