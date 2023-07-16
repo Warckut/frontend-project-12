b6c06a253b38ec7ca451e72a9372e8f0f90729a0
warckut <warckut@gmail.com>
2023-07-16 19:35:36 +0500

    fix

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -8,8 +8,6 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(userData);
 
   const logIn = ({ token, username }) => {
-    console.log(token);
-    console.log(username);
     saveUserData({ token, username });
     setLoggedIn(true);
     setUser({ username });
